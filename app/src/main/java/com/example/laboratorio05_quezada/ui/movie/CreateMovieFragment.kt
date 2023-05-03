9cdd9fcb64ba6881b39b4249d891ef48733559e4
Rafael <00084021@uca.edu.sv>
2023-05-03 11:02:06 -0600

    Laboratorio 7 inicio

--- app/src/main/java/com/example/laboratorio05_quezada/ui/movie/CreateMovieFragment.kt
+++ app/src/main/java/com/example/laboratorio05_quezada/ui/movie/CreateMovieFragment.kt
@@ -39,7 +39,7 @@ class CreateMovieFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         bind(view)
-        actionSubmit.setOnClickListener{
+        actionSubmit.setOnClickListener{ 
         createMovie()
         }
     }
